3aaa7ba1996ff4f285385f7f9629b3b467452d0c
lichv <lichvg@gmail.com>
2016-11-30 01:39:52 +0800

    Add files via upload

---
+++ src/Uploader/UploadAlioss.php
@@ -0,0 +1,31 @@
+<?php
+namespace Lichv\UEditor\Uploader;
+
+use OSS\OssClient;
+use OSS\Core\OssException;
+
+/**
+ *
+ *
+ * trait UploadAlioss
+ *
+ * 阿里OSS 上传 类
+ *
+ * @package Lichv\UEditor\Uploader
+ */
+trait UploadAlioss
+{
+    /**
+     * 获取文件路径
+     * @return string
+     */
+
+    public function UploadAlioss($key, $content){
+        $ossClient = new OssClient(config('UEditorUpload.core.alioss.appId'), config('UEditorUpload.core.alioss.appKey'), config('UEditorUpload.core.alioss.host'));
+        $filename = 'uploads/'.date("Y/m/d/",time()).$key;
+        $result =$ossClient->uploadFile(config('UEditorUpload.core.alioss.bucket'),$filename,$content);
+        $this->fullName = $url = 'http://'.config('UEditorUpload.core.alioss.bucket').'.'.config('UEditorUpload.core.alioss.host').'/'.$filename;
+        $this->stateInfo = $this->stateMap[0];
+        return true;
+    }
+}
